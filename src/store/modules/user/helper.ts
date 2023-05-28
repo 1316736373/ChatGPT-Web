fac57f9195f02b03a78195fb7fdedbc95e13a17d
1316736373 <43538093+1316736373@users.noreply.github.com>
2023-05-28 20:25:23 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,7 +15,7 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://worldgz.photo.store.qq.com/psc?/worldlogo/O0cFsaTfOlqjIAnYAvw8WjVg226xOzHJrHqLHhAR.XxFOHBZQ.qf*ryetBTdBtm6BUodbd4wKDWfBOdRwEnFuB628N3WATWNamubswMlm80!/m&bo=gAKAAoACgAIRHyg!',
+      avatar: 'https://s2.xptou.com/2023/05/28/647347479c2a2.jpg',
       name: 'QQ群：741785402',
       description: 'Star on <a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=votE2iRETpMV_KcLTqCBw9oqM22S4zMb&authKey=6%2FljpbebmrzPdOXSmIuOdxiHWiqabYY1wYzcRzu31Kwz7N7EG1wIdeJnMlBwYoMH&noverify=0&group_code=741785402" class="text-blue-500" target="_blank" >QQ群：741785402</a>',
     },
