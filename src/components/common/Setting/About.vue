a4ac34756d279840174c90a7940e3b384e8fa86d
1316736373 <43538093+1316736373@users.noreply.github.com>
2023-05-28 19:50:59 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -49,7 +49,7 @@ onMounted(() => {
           QQ群：741785402
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
+            href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=GQo53jmMQWNP2brrIf7tn6gYCiG50_eT&authKey=rHu%2BPM3XUbQZQqqZE%2FxOZ4pey6AuA%2F4INnkFVn267c%2Fpcsb9XeGOQNShB%2BoB3S%2Bt&noverify=0&group_code=741785402"
             target="_blank"
           >
             QQ群：741785402
