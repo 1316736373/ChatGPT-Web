02f3f68471ad141f9bc0c653318f234e63e21e12
1316736373 <43538093+1316736373@users.noreply.github.com>
2023-11-02 12:00:28 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-neutral-400">
     <span>Star on</span>
-    <a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=GQo53jmMQWNP2brrIf7tn6gYCiG50_eT&authKey=rHu%2BPM3XUbQZQqqZE%2FxOZ4pey6AuA%2F4INnkFVn267c%2Fpcsb9XeGOQNShB%2BoB3S%2Bt&noverify=0&group_code=741785402" target="_blank" class="text-blue-500"
+    <a href="https://yan.changxunwangluo.cn/huichuan/cd.php" target="_blank" class="text-blue-500"
 		免费
     </a>
   </div>
